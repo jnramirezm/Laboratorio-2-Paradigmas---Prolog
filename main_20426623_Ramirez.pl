987d51e260ca679af694af8ff4f055df976d7658
Juan Ramirez <juan.ramirez.m@usach.cl>
2022-05-01 20:02:18 -0400

    Creacion NNCartas y mazo.

--- main_20426623_Ramirez.pl
+++ main_20426623_Ramirez.pl
@@ -1,21 +1,18 @@
 % Primera carta
-fcard(L, N):-
-        fcard(L, N, 1).
+fcard(N, L):-
+        fcard(N, 1, L).
 
-fcard([], N, X) :-
-    X>N,
-    !.
+fcard(N, X,[X]) :-
+    X > N.
 
-fcard([X|L], N, X):-
-        X =< N,
+fcard(N, X, [X|L]):-
+        N >= X,
         X1 is X + 1,
-        fcard(L, N, X1).
-
+        fcard(N, X1, L).
 
 % Primeras N cartas
 
 ncards(N,J,L):-
-    	J1 is J+1,
         ncards(N, J, 1, 1, L).
 
 ncards(N, J, K, L, [L]):-
@@ -39,3 +36,58 @@ ncards2(N,J,L1,[L1|L]):-
         ncards(N,J,L2),
         J1 is J+1,
         ncards2(N, J1, L2, L).
+
+%nncards
+
+nncards4(N,K,I,J,L):- 
+    X1 is I+1,
+    nncards4(N,K,I,J,X1,L).
+
+nncards4(N,K,I,J,L,[L]):-
+    K > N.
+
+nncards4(N,K,I,J,X1,[X1|L]):-
+    N >= K,
+    K1 is K+1,
+    X2 is (N+2+N*(K-1)+(((I-1)*(K-1)+J-1) mod N)),
+    nncards4(N,K1,I,J,X2,L).
+
+nncards3(N,I,J,L):-
+	nncards4(N,1,I,J,L1),
+	nncards3(N,I,J,L1,L).
+
+nncards3(N,I,J,L,[L]):-
+    J >= N.
+
+nncards3(N,I,J,L1,[L1|L]):-
+    N >= J,
+    J1 is J+1,
+    nncards4(N,1,I,J1,L2),
+    nncards3(N,I,J1,L2,L).
+
+nncards2(N,I,L):-
+    nncards3(N,I,1,L1),
+    nncards2(N,I,L1,L).
+
+nncards2(N,I,L,[L]):-
+    I >= N.
+
+nncards2(N,I,L1,L):-
+    N >= I,
+    I1 is I + 1,
+    nncards3(N,I1,1,L2),
+    append(L1,L2,L3),
+    nncards2(N,I1,L3,L).
+
+nncards(N,L):-
+    nncards2(N,1,[X|L1]),
+    append(L1,X,L).
+
+% Creacion mazo
+
+mazo(N,L):-
+    fcard(N, L1),
+    ncards2(N,L2),
+    append([L1],L2,L3),
+    nncards(N,L4),
+    append(L3,L4,L).
